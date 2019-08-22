c852150d1e7fe810df1c33d937e758d01dc9a460
allenj12 <joeallen92@gmail.com>
2019-08-22 18:57:14 -0400

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to counterstakes
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/counterstakes/core_test.clj
@@ -0,0 +1,7 @@
+(ns counterstakes.core-test
+  (:require [clojure.test :refer :all]
+            [counterstakes.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
